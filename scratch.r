f94342a391f4f81f8109f4253a85d694e98d7a6f
Michael Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2025-06-13 11:56:47 -0700

    working example

--- scratch.r
+++ scratch.r
@@ -2,50 +2,38 @@ requireNamespace("jsonlite")
 library(sf)
 library(ggplot2)
 library(ggspatial)
+
 source("sdf_to_geojson.r")
 
-setwd("C:/Users/G2ECHMCK/Workspace/Scratch/Carpenter_Creek (113)")
-
-outfile = "carpenter_creek_profiles.gpkg"
+sdf_file = "example.sdf"
+outfile = "profiles.gpkg"
 unlink(outfile)
 
-sdf_files = list(
-  "500yr" = "Simulations/carpenter_creek_ras.RASexport.500yr.sdf",
-  "100yr" = "Simulations/carpenter_creek_ras.RASexport.100yr.sdf"
-)
+# parse sdf file
+all_xs = parse_sdf(sdf_file, what = "cross sections",
+  geometry = "surface line")
 
-for (i in seq_along(sdf_files)) {
-  sdf_file = sdf_files[[i]]
-  layer_name = names(sdf_files)[i]
+all_reaches = parse_sdf(sdf_file, what = "stream network")
 
-  # parse sdf file
-  all_xs = parse_sdf(sdf_file, what = "cross sections",
-    geometry = "surface line")
+# convert to geojson string
+xs_json = to_geojson(all_xs)
+reach_json = to_geojson(all_reaches)
 
-  # convert to geojson string
-  xs_json = xs_to_geojson(all_xs)
+# convert to sf object
+xs_polylines = st_read(xs_json, crs = 4430)
+reach_polylines = st_read(reach_json, crs = 4430)
 
-  # convert to sf object
-  rr = st_read(xs_json, crs = 4430)
+# convert z-polylines to XYZ points
+xs_points = xs_polylines |>
+  st_cast("MULTIPOINT") |>
+  st_cast("POINT")
+# add some attributes
+xs_points["X"] = st_coordinates(points)[, 1]
+xs_points["Y"] = st_coordinates(points)[, 2]
+xs_points["Elev"] = st_coordinates(points)[, 3]
+xs_points["Below_WSE"] = points$Elev < points$water_elevation
 
-
-  # convert z-polylines to XYZ points
-  points = polylines |>
-    st_cast("MULTIPOINT") |>
-    st_cast("POINT")
-  # add some attributes
-  points["X"] = st_coordinates(points)[,1]
-  points["Y"] = st_coordinates(points)[,2]
-  points["Elev"] = st_coordinates(points)[,3]
-  points["Below_WSE"] = points$Elev < points$water_elevation
-
-  # write points layer to geopackage
-  points |>
-    st_write(outfile, layer_name)
-
-  # plot with ggplot
-  #ggplot(points) +
-  #  annotation_map_tile(zoom = 15) +
-  #  geom_sf()
-
-}
+# write layers to geopackage
+st_write(xs_points, outfile, "xs_points")
+st_write(xs_polylines, outfile, "xs_lines")
+st_write(reach_polylines, outfile, "reach_lines")
